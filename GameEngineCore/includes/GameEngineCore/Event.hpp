ed18cfc1612fc95145b7f725ac9e767a178ed63f
Vladimir <arhitektor_v@mail.ru>
2023-08-14 15:30:33 +0700

    Added event handling

---
+++ GameEngineCore/includes/GameEngineCore/Event.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace GameEngine {
+
+	struct Event
+	{
+		unsigned int width;
+		unsigned int height;
+	};
+
+}
--- GameEngineCore/src/GameEngineCore/Window.cpp
+++ GameEngineCore/src/GameEngineCore/Window.cpp
@@ -9,9 +9,7 @@ namespace GameEngine {
     static bool s_GLFW_initialized = false;
 
 	Window::Window(std::string title, const unsigned int width, const unsigned int height)
-		:m_title(std::move(title))
-		, m_width(width)
-		, m_height(height)
+		: m_data({std::move(title), width, height})        
 	{
 		int resultCode = init();
 	}
@@ -23,7 +21,7 @@ namespace GameEngine {
 
 	int Window::init()
 	{
-        LOG_INFO("Creating window '{0}' width size {1}x{2}", m_title, m_width, m_height);
+        LOG_INFO("Creating window '{0}' width size {1}x{2}", m_data.title, m_data.width, m_data.height);
 
         if (!s_GLFW_initialized)
         {
@@ -37,10 +35,10 @@ namespace GameEngine {
         
 
       
-        m_pWindow = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
+        m_pWindow = glfwCreateWindow(m_data.width, m_data.height, m_data.title.c_str(), nullptr, nullptr);
         if (!m_pWindow)
         {
-            LOG_CRITICAL("Can't create window {0} width size {1}x{2}", m_title, m_width, m_height);
+            LOG_CRITICAL("Can't create window {0} width size {1}x{2}", m_data.title, m_data.width, m_data.height);
             glfwTerminate();
             return -2;
         }
@@ -54,6 +52,25 @@ namespace GameEngine {
             LOG_CRITICAL("Failed to initialize GLAD");
             return -3;
         }
+
+        glfwSetWindowUserPointer(m_pWindow, &m_data);
+
+        glfwSetWindowSizeCallback(m_pWindow,
+            [](GLFWwindow* pWindow, int width, int height)
+            {
+                LOG_INFO("New size {0}x{1}", width, height);
+
+                WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(pWindow));
+                data.width = width;
+                data.height = height;
+
+                Event event;
+                event.width = width;
+                event.height = height;
+                data.eventCallbackFn(event);
+
+            }
+        );
 
         return 0;
             
